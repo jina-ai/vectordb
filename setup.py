0f8561929568ce971cf6c9f0f39f536b1400d5b7
Joan Fontanals Martinez <joan.martinez@jina.ai>
2023-05-29 14:51:43 +0200

    chore: start the bones of the project
    Signed-off-by: Joan Fontanals Martinez <joan.martinez@jina.ai>

---
+++ setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+from vectordb import __version__
+
+# Read the contents of requirements.txt
+with open('requirements.txt', 'r') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='vectordb',
+    version=__version__,
+    description='The Python VectorDB. Build your vector database from working as a library to scaling as a database in the cloud',
+    author='Jina AI',
+    author_email='hello@jina.ai',
+    license='Apache 2.0',
+    url='https://github.com/jina-ai/vectordb/',
+    download_url='https://github.com/jina-ai/vectordb/tags',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+    ],
+    python_requires='>=3.7',
+    extras_require={
+        'test': [
+            'pytest',
+            'pytest-asyncio',
+        ],
+    },
+    install_requires=requirements,
+)
