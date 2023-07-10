c66668c902463271178bbd84b6271c93b3619b15
Joan Fontanals Martinez <joan.martinez@jina.ai>
2023-07-10 15:17:39 +0200

    docs: fix README

--- vectordb/__init__.py
+++ vectordb/__init__.py
@@ -2,3 +2,4 @@ __version__ = '0.0.6'
 
 from vectordb.db.inmemory_exact_vectordb import InMemoryExactNNVectorDB
 from vectordb.db.hnsw_vectordb import HNSWVectorDB
+from vectordb.client import Client
