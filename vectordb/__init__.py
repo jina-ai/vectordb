609a03f41f6eb64d7f9d36eb2cd9874f7fafa6bb
Deepankar Mahapatro <deepankar.mahapatro@jina.ai>
2023-07-14 12:32:20 +0530

    chore: ignore warnings

--- vectordb/__init__.py
+++ vectordb/__init__.py
@@ -1,5 +1,19 @@
-__version__ = '0.0.12'
+def _ignore_warnings():
+    import logging
+    import warnings
 
-from vectordb.db.inmemory_exact_vectordb import InMemoryExactNNVectorDB
-from vectordb.db.hnsw_vectordb import HNSWVectorDB
+    logging.captureWarnings(True)
+    warnings.filterwarnings(
+        "ignore",
+        category=DeprecationWarning,
+        message="Deprecated call to `pkg_resources.declare_namespace('google')`.",
+    )
+
+
+_ignore_warnings()
+
+__version__ = "0.0.12"
+
 from vectordb.client import Client
+from vectordb.db.hnsw_vectordb import HNSWVectorDB
+from vectordb.db.inmemory_exact_vectordb import InMemoryExactNNVectorDB
