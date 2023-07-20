5d6b6df1739c86832273820c89022926dea051aa
Joan Fontanals Martinez <joan.martinez@jina.ai>
2023-07-20 10:22:32 +0200

    test: optout telemetry in test

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,11 @@
 import pytest
 
+
 @pytest.fixture(autouse=True)
 def test_log_level(monkeypatch):
     monkeypatch.setenv('JINA_LOG_LEVEL', 'DEBUG')
+
+
+@pytest.fixture(autouse=True)
+def test_disable_telemetry(monkeypatch):
+    monkeypatch.setenv('JINA_OPTOUT_TELEMETRY', 'True')
